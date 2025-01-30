e5240f66b0807834af3024663a870766f5d23bb9
habibrolandt <habibrolandt@gmail.com>
2025-01-30 10:53:39 +0000

    first commit

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
 // Exercice 1
-const  calc = (array, n1, n2)=>{
+const calc = (array, n1, n2) => {
     let sum = 0;
-    for(let i=0; i<=n2; i++){
-        sum += array[i]
+    if (n1 >= 0 && n1 <= n2 && n2 < array.length
+        && n1 < n2) {
+        for (let i = n1; i <= n2; i++) {
+            sum += array[i];
+        }
     }
-    return sum
+    return sum;
 }
-console.log(calc([1,2,3,5, 110], 0, 4))
+console.log(calc([1, 2, 3, 4, 5, 6], 1, 4))
 
 
-// Exercice 2
+
+
+// Gestion des taches (Exercice 2)
+
+const ajouteTache = () => {
+    let tache = document.getElementById("tache").value;
+    let listeTache = document.getElementById("listeTache");
+    let li = document.createElement("li");
+    li.innerHTML = tache;
+    listeTache.appendChild(li);
+}
