85f4feac6be1a2d5aca6383d855f0e141a3e4fff
habibrolandt <habibrolandt@gmail.com>
2025-01-30 10:25:20 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+// Exercice 1
+const  calc = (array, n1, n2)=>{
+    let sum = 0;
+    for(let i=0; i<=n2; i++){
+        sum += array[i]
+    }
+    return sum
+}
+console.log(calc([1,2,3,5, 110], 0, 4))
+
+
+// Exercice 2
